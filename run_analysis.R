174d770be810b0a6212ef569f6cf6cdc0ef5ecc0
Juan Navarro <juan.navarro.horniacek@gmail.com>
2015-02-21 05:21:19 -0300

    R script updated with comment and aux variable so it can be easier to understand

--- run_analysis.R
+++ run_analysis.R
@@ -33,7 +33,7 @@ data <- cbind(x_data, y_data, subject_data)
 # Create a second, independent tidy data set with the average of each variable
 # for each activity and each subject and Write into a file
 
-last_column <- ncol(data) - 2 # The last two columns cannot be averages (activity, subject)
+last_column <- ncol(data) - 2 # The last two columns cannot be averaged (activity, subject)
 data_averages <- ddply(data, .(subject, activity), function(x) colMeans(x[, seq(1,last_column)]))
 write.table(data_averages, "data_averages.txt", row.name=FALSE)
 
